06102693e6059be2d90fa22a135d122518ac33a0
alif33 <alifhasan332@gmail.com>
2023-08-13 00:03:43 +0600

    added static folder

--- constants/index.ts
+++ constants/index.ts
@@ -54,7 +54,7 @@ export const services = [
     titleColor: 'text-colorfull-red',
     captionColor: 'text-colorfull-blue',
     title: 'App Development',
-    imageUrl: '/images/service-app-development.jpg',
+    imageUrl: '/static/images/service-app-development.jpg',
     caption: 'Tailor-Made Apps for Your Unique Brand',
     content: `We know your app is as unique as your brand, and that's why our app development solutions are tailor-made to suit your specific requirements and goals. Our collaborative process ensures your vision remains at the forefront of our development, resulting in an app that's truly a reflection of your brand.`
   },
@@ -138,37 +138,37 @@ export const contacts = [
 export const works = [
   {
     id: 'work-1',
-    imageUrl: '/images/works/app-development.webp',
+    imageUrl: '/static/images/works/app-development.webp',
     type: 'App Development',
     title: 'Title'
   },
   {
     id: 'work-2',
-    imageUrl: '/images/works/digital-marketing-seo.jpg',
+    imageUrl: '/static/images/works/digital-marketing-seo.jpg',
     type: 'Digital Marketing & SEO',
     title: 'Title'
   },
   {
     id: 'work-3',
-    imageUrl: '/images/works/graphic-design.jpg',
+    imageUrl: '/static/images/works/graphic-design.jpg',
     type: 'Graphic Design',
     title: 'Title'
   },
   {
     id: 'work-4',
-    imageUrl: '/images/works/social-media.jpg',
+    imageUrl: '/static/images/works/social-media.jpg',
     type: 'Soc-Med Management',
     title: 'Title'
   },
   {
     id: 'work-5',
-    imageUrl: '/images/works/video-editing.webp',
+    imageUrl: '/static/images/works/video-editing.webp',
     type: 'Video Editing',
     title: 'Title'
   },
   {
     id: 'work-6',
-    imageUrl: '/images/works/web-design.png',
+    imageUrl: '/static/images/works/web-design.png',
     type: 'Web Design',
     title: 'Title'
   },

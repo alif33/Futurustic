734d1095be45974ba59c2d0b7522813bd90c6c6b
alif33 <alifhasan332@gmail.com>
2023-08-12 23:46:20 +0600

    remove config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['localhost', 'https://futurusticdesigners.com/'],
-    },
 }
 
 module.exports = nextConfig
